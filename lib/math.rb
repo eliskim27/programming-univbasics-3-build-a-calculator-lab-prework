7feac901aba707d2ae83cb173b099b1eccff3e81
Eli Kim <eliskim27@gmail.com>
2019-08-28 19:39:57 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,25 +5,25 @@ end
 
 def subtraction(num1, num2)
   answer = num1 - num2
-  puts answer
+  return answer
 end
 
 def division(num1, num2)
   answer = num1 / num2
-  puts answer
+  return answer
 end
 
 def multiplication(num1, num2)
   answer = num1 * num2
-  puts answer
+  return answer
 end
 
 def modulo(num1, num2)
   answer = num1 % num2
-  puts answer
+  return answer
 end
 
 def square_root(num)
   answer = num.sqrt
-  puts answer
+  return answer
 end
